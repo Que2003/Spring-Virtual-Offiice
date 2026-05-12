3bacdf706274a2267ce18f140c30526629adc741
Que2003 <dillingsq2003@gmail.com>
2026-05-12 10:34:54 -0500

    Add files via upload

--- run_discord_bot.py
+++ run_discord_bot.py
@@ -1,4 +1,9 @@
-from app.discord_bot.bot import run_bot
+"""
+Entry point for Discord bot
+Run: python run_discord_bot.py
+"""
+
+from app.discord_bot.bot_enhanced import run_bot
 
 if __name__ == "__main__":
     run_bot()
